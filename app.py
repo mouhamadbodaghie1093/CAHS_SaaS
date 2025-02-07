7185c585d8f2c3df128f701f047b1cbb6c5ad7e2
mouhamadbodaghie <Mouhamadbodaghie@gmail.com>
2025-02-07 01:46:11 +0000

    Add files via upload
    Signed-off-by: mouhamadbodaghie <Mouhamadbodaghie@gmail.com>

--- app.py
+++ app.py
@@ -223,4 +223,4 @@ def health_check():
 # ---------------- RUN APP ---------------- #
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=10000, debug=False)
